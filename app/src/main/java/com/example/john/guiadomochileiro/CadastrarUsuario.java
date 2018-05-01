ede40c66c2ee664271223c0294c7a06f7a62051e
John <johnkssilva@gmail.com>
2018-05-01 17:14:19 -0300

    Second commit

---
+++ app/src/main/java/com/example/john/guiadomochileiro/CadastrarUsuario.java
@@ -0,0 +1,50 @@
+package com.example.john.guiadomochileiro;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CadastrarUsuario extends AppCompatActivity {
+
+    public static EditText usuario, email, senha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_usuario);
+    }
+
+    public void cadastrarU(View view) {
+
+        EditText edUsuario = (EditText)findViewById(R.id.etUsuario);
+        EditText edEmail = (EditText)findViewById(R.id.etEmail);
+        EditText edSenha = (EditText)findViewById(R.id.etSenha);
+        String usuario = edUsuario.getText().toString();
+        String email = edEmail.getText().toString();
+        String senha = edSenha.getText().toString();
+
+        if(edUsuario.getText().length() == 0){
+            edUsuario.setError("Campo vazio");
+        }
+        if(edEmail.getText().length() == 0) {
+            edEmail.setError("Campo vazio");
+        }
+        if(edSenha.getText().length() == 0){
+            edSenha.setError("Campo vazio");
+        }
+
+        if((usuario.length() > 4) && (email.length() > 12) && (senha.length() > 4) ) {
+            Context contexto = getApplicationContext();
+            String texto = "Usuário cadastrado com sucesso";
+            int duracao = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+            finish();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/john/guiadomochileiro/Login.java
@@ -0,0 +1,59 @@
+package com.example.john.guiadomochileiro;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    public static EditText usuario, senha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void cadastrarUsuario(View view) {
+        Intent cadastrarUsuario = new Intent(this, CadastrarUsuario.class);
+        startActivity(cadastrarUsuario);
+    }
+    public void loginUsuario(View view) {
+
+        EditText edUsuario = (EditText) findViewById(R.id.etUsuario);
+        EditText edSenha = (EditText) findViewById(R.id.etSenha);
+        String usuario = edUsuario.getText().toString();
+        String senha = edSenha.getText().toString();
+
+        if(edUsuario.getText().length() == 0){
+            edUsuario.setError("Campo vazio");
+        }
+        if(edSenha.getText().length() == 0){
+            edSenha.setError("Campo vazio");
+        }
+
+        if((usuario.equals("adm")) && (senha.equals("adm"))){
+            Context contexto = getApplicationContext();
+            String texto = "Login efetuado sucesso";
+            int duracao = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+
+            Intent login = new Intent(this, Inicio.class);
+            startActivity(login);
+        }
+        else{
+            Context contexto = getApplicationContext();
+            String texto = "Login ou senha invalido";
+            int duracao = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(contexto, texto,duracao);
+            toast.show();
+        }
+    }
+}
